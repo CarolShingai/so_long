471f6d1f4e8e8f1cba3cceb1d1a4370c84782476
Carol Shingai <carolineshingai@gmail.com>
2024-03-05 12:32:22 -0300

    imagens

--- srcs/create_img.c
+++ srcs/create_img.c
@@ -6,7 +6,7 @@
 /*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 17:51:01 by cshingai          #+#    #+#             */
-/*   Updated: 2024/02/29 16:55:39 by cshingai         ###   ########.fr       */
+/*   Updated: 2024/03/05 02:03:00 by cshingai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,10 @@ mlx_image_t	*create_img(mlx_t *mlx, char *img_path)
 		ft_error(IMAGE_ERROR);
 	mlx_delete_texture(texture);
 	return (img);
+}
+
+void	insert_img(t_game game)
+{
+	game.img.floor = create_img(game.mlx, "./sprites/grass_tile.png");
+	game.img.wall = create_img(game.mlx, "./sprites/block_tile.png");
 }
--- srcs/init_game.c
+++ srcs/init_game.c
@@ -6,7 +6,7 @@
 /*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 19:03:41 by cshingai          #+#    #+#             */
-/*   Updated: 2024/03/01 19:11:17 by cshingai         ###   ########.fr       */
+/*   Updated: 2024/03/05 00:08:01 by cshingai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	init_game(t_game *game)
 {
-	game->mlx = mlx_init(WIDTH, HEIGHT, "so_long", true);
+	game->mlx = mlx_init(game->map.width * TILE, game->map.height * TILE, "so_long", true);
+	if(!game->mlx)
+		ft_error(SOME_ERROR);
+	create_img
 }
